9cb26b4d8894b8e0b2a97389d1f28b7b3b1f272b
Glitch (kontolpecah) <none>
2020-10-08 20:42:34 +0000

    maah

---
+++ index.js
@@ -0,0 +1,500 @@
+const { Client, Util, MessageEmbed } = require("discord.js");
+const YouTube = require("simple-youtube-api");
+const ytdl = require("ytdl-core");
+require("dotenv").config();
+require("./server.js");
+
+const bot = new Client({
+    disableMentions: "all"
+});
+
+const PREFIX = process.env.PREFIX;
+const youtube = new YouTube(process.env.YTAPI_KEY);
+const queue = new Map();
+
+bot.on("warn", console.warn);
+bot.on("error", console.error);
+bot.on("ready", () => console.log(`[READY] ${bot.user.tag} has been successfully booted up!`));
+bot.on("shardDisconnect", (event, id) => console.log(`[SHARD] Shard ${id} disconnected (${event.code}) ${event}, trying to reconnect...`));
+bot.on("shardReconnecting", (id) => console.log(`[SHARD] Shard ${id} reconnecting...`));
+
+bot.on("message", async (message) => { // eslint-disable-line
+    if (message.author.bot) return;
+    if (!message.content.startsWith(PREFIX)) return;
+
+    const args = message.content.split(" ");
+    const searchString = args.slice(1).join(" ");
+    const url = args[1] ? args[1].replace(/<(.+)>/g, "$1") : "";
+    const serverQueue = queue.get(message.guild.id);
+
+    let command = message.content.toLowerCase().split(" ")[0];
+    command = command.slice(PREFIX.length);
+
+    if (command === "help" || command === "cmd") {
+        const helpembed = new MessageEmbed()
+            .setColor("BLUE")
+            .setAuthor(bot.user.tag, bot.user.displayAvatarURL())
+            .setDescription(`
+__**Command list**__
+> \`play\` > **\`play [title/url]\`**
+> \`search\` > **\`search [title]\`**
+> \`skip\`, \`stop\`,  \`pause\`, \`resume\`
+> \`nowplaying\`, \`queue\`, \`volume\``)
+            .setFooter("Credit to ©️ 2020 Zhycorp Nation", "https://api.zhycorp.xyz/assets/images/icon.jpg");
+        message.channel.send(helpembed);
+    }
+    if (command === "play" || command === "p") {
+        const voiceChannel = message.member.voice.channel;
+        if (!voiceChannel) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "Maaf gan lo belum ada di Voice Channel, Join dulu yak!"
+            }
+        });
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if (!permissions.has("CONNECT")) {
+            return message.channel.send({
+                embed: {
+                    color: "RED",
+                    description: "Maaf bos, gw butuh **`CONNECT`** buat jalan!"
+                }
+            });
+        }
+        if (!permissions.has("SPEAK")) {
+            return message.channel.send({
+                embed: {
+                    color: "RED",
+                    description: "Maaf bos, gw butuh **`SPEAK`** buat jalan!"
+                }
+            });
+        }
+        if (!url || !searchString) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "Harap masukan link/judul buat jalanin musicnya"
+            }
+        });
+        if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+            const playlist = await youtube.getPlaylist(url);
+            const videos = await playlist.getVideos();
+            for (const video of Object.values(videos)) {
+                const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
+                await handleVideo(video2, message, voiceChannel, true); // eslint-disable-line no-await-in-loop
+            }
+            return message.channel.send({
+                embed: {
+                    color: "GREEN",
+                    description: `✅  **|**  Playlist: **\`${playlist.title}\`** sudah di masukan kedalam queue`
+                }
+            });
+        } else {
+            try {
+                var video = await youtube.getVideo(url);
+            } catch (error) {
+                try {
+                    var videos = await youtube.searchVideos(searchString, 10);
+                    var video = await youtube.getVideoByID(videos[0].id);
+                    if (!video) return message.channel.send({
+                        embed: {
+                            color: "RED",
+                            description: "🆘  **|**  gw engga nemuin apa yang lo cari bos!"
+                        }
+                    });
+                } catch (err) {
+                    console.error(err);
+                    return message.channel.send({
+                        embed: {
+                            color: "RED",
+                            description: "🆘  **|**  Gw engga dapat memperoleh hasil pencarian apa pun"
+                        }
+                    });
+                }
+            }
+            return handleVideo(video, message, voiceChannel);
+        }
+    }
+    if (command === "search" || command === "sc") {
+        const voiceChannel = message.member.voice.channel;
+        if (!voiceChannel) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "Maaf gan lo belum ada di Voice Channel, Join dulu yak!"
+            }
+        });
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if (!permissions.has("CONNECT")) {
+            return message.channel.send({
+                embed: {
+                    color: "RED",
+                    description: "Maaf bos, gw butuh **`CONNECT`** buat jalan!"
+                }
+            });
+        }
+        if (!permissions.has("SPEAK")) {
+            return message.channel.send({
+                embed: {
+                    color: "RED",
+                    description: "Maaf bos, gw butuh **`SPEAK`** buat jalan!"
+                }
+            });
+        }
+        if (!url || !searchString) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "Harap masukan link/judul buat jalanin musicnya"
+            }
+        });
+        if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+            const playlist = await youtube.getPlaylist(url);
+            const videos = await playlist.getVideos();
+            for (const video of Object.values(videos)) {
+                const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
+                await handleVideo(video2, message, voiceChannel, true); // eslint-disable-line no-await-in-loop
+            }
+            return message.channel.send({
+                embed: {
+                    color: "GREEN",
+                    description: `✅  **|**  Playlist: **\`${playlist.title}\`** udah gw masukin queue bos ku`
+                }
+            });
+        } else {
+            try {
+                var video = await youtube.getVideo(url);
+            } catch (error) {
+                try {
+                    var videos = await youtube.searchVideos(searchString, 10);
+                    let index = 0;
+                    let embedPlay = new MessageEmbed()
+                        .setColor("BLUE")
+                        .setAuthor("Search results", message.author.displayAvatarURL())
+                        .setDescription(`${videos.map(video2 => `**\`${++index}\`  |**  ${video2.title}`).join("\n")}`)
+                        .setFooter("Please choose one of the following 10 results, this embed will auto-deleted in 15 seconds");
+                    // eslint-disable-next-line max-depth
+                    message.channel.send(embedPlay).then(m => m.delete({
+                        timeout: 15000
+                    }))
+                    try {
+                        var response = await message.channel.awaitMessages(message2 => message2.content > 0 && message2.content < 11, {
+                            max: 1,
+                            time: 15000,
+                            errors: ["time"]
+                        });
+                    } catch (err) {
+                        console.error(err);
+                        return message.channel.send({
+                            embed: {
+                                color: "RED",
+                                description: "The song selection time has expired in 15 seconds, the request has been canceled."
+                            }
+                        });
+                    }
+                    const videoIndex = parseInt(response.first().content);
+                    var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+                } catch (err) {
+                    console.error(err);
+                    return message.channel.send({
+                        embed: {
+                            color: "RED",
+                            description: "🆘  **|**  gw engga nemuin apa apa"
+                        }
+                    });
+                }
+            }
+            response.delete();
+            return handleVideo(video, message, voiceChannel);
+        }
+
+    } else if (command === "skip") {
+        if (!message.member.voice.channel) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "ngeskip truuuuus buat apa? lo belum di voice channel juga"
+            }
+        });
+        if (!serverQueue) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "Lo belum ngasih lagu, mau apaan yg gw skip"
+            }
+        });
+        serverQueue.connection.dispatcher.end("[runCmd] Skip command has been used");
+        return message.channel.send({
+            embed: {
+                color: "GREEN",
+                description: "⏭️  **|**  udah gw skip noh"
+            }
+        });
+
+    } else if (command === "stop") {
+        if (!message.member.voice.channel) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "Ngapain? lo aja belum di voice channel udah nyuruh ngestop aja"
+            }
+        });
+        if (!serverQueue) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "ga ada lagu buat di mainin"
+            }
+        });
+        serverQueue.songs = [];
+        serverQueue.connection.dispatcher.end("[runCmd] Stop command has been used");
+        return message.channel.send({
+            embed: {
+                color: "GREEN",
+                description: "⏹️  **|**  Ngapus queue ama keluar voice channel"
+            }
+        });
+
+    } else if (command === "volume" || command === "vol") {
+        if (!message.member.voice.channel) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "Lo aja belum ada di Voice Channel buat ngecil gedein suara gw"
+            }
+        });
+        if (!serverQueue) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "Ga ada bahan buat di mainin"
+            }
+        });
+        if (!args[1]) return message.channel.send({
+            embed: {
+                color: "BLUE",
+                description: `Volume nya segini: **\`${serverQueue.volume}%\`**`
+            }
+        });
+        if (isNaN(args[1]) || args[1] > 100) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "Volume hanya bisa di kisaran **\`1\`** - **\`100\`**"
+            }
+        });
+        serverQueue.volume = args[1];
+        serverQueue.connection.dispatcher.setVolume(args[1] / 100);
+        return message.channel.send({
+            embed: {
+                color: "GREEN",
+                description: `gw dah set volume ke: **\`${args[1]}%\`**`
+            }
+        });
+
+    } else if (command === "nowplaying" || command === "np") {
+        if (!serverQueue) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "au ah gelap"
+            }
+        });
+        return message.channel.send({
+            embed: {
+                color: "BLUE",
+                description: `🎶  **|**  Lagi jalanin video/lagu ini bos: **\`${serverQueue.songs[0].title}\`**`
+            }
+        });
+
+    } else if (command === "queue" || command === "q") {
+
+        let songsss = serverQueue.songs.slice(1)
+        
+        let number = songsss.map(
+            (x, i) => `${i + 1} - ${x.title}`
+        );
+        number = chunk(number, 5);
+
+        let index = 0;
+        if (!serverQueue) return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "wah gelap"
+            }
+        });
+        let embedQueue = new MessageEmbed()
+            .setColor("BLUE")
+            .setAuthor("Song queue", message.author.displayAvatarURL())
+            .setDescription(number[index].join("\n"))
+            .setFooter(`• Now Playing: ${serverQueue.songs[0].title} | Page ${index + 1} of ${number.length}`);
+        const m = await message.channel.send(embedQueue);
+
+        if (number.length !== 1) {
+            await m.react("⬅");
+            await m.react("🛑");
+            await m.react("➡");
+            async function awaitReaction() {
+                const filter = (rect, usr) => ["⬅", "🛑", "➡"].includes(rect.emoji.name) &&
+                    usr.id === message.author.id;
+                const response = await m.awaitReactions(filter, {
+                    max: 1,
+                    time: 30000
+                });
+                if (!response.size) {
+                    return undefined;
+                }
+                const emoji = response.first().emoji.name;
+                if (emoji === "⬅") index--;
+                if (emoji === "🛑") m.delete();
+                if (emoji === "➡") index++;
+
+                if (emoji !== "🛑") {
+                    index = ((index % number.length) + number.length) % number.length;
+                    embedQueue.setDescription(number[index].join("\n"));
+                    embedQueue.setFooter(`Page ${index + 1} of ${number.length}`);
+                    await m.edit(embedQueue);
+                    return awaitReaction();
+                }
+            }
+            return awaitReaction();
+        }
+
+    } else if (command === "pause") {
+        if (serverQueue && serverQueue.playing) {
+            serverQueue.playing = false;
+            serverQueue.connection.dispatcher.pause();
+            return message.channel.send({
+                embed: {
+                    color: "GREEN",
+                    description: "⏸  **|**  udah gw pause tuh"
+                }
+            });
+        }
+        return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "njir masih gelap"
+            }
+        });
+
+    } else if (command === "resume") {
+        if (serverQueue && !serverQueue.playing) {
+            serverQueue.playing = true;
+            serverQueue.connection.dispatcher.resume();
+            return message.channel.send({
+                embed: {
+                    color: "GREEN",
+                    description: "▶  **|**  Lanjut goyang bos"
+                }
+            });
+        }
+        return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "goyang hening mau?"
+            }
+        });
+    } else if (command === "loop") {
+        if (serverQueue) {
+            serverQueue.loop = !serverQueue.loop;
+            return message.channel.send({
+                embed: {
+                    color: "GREEN",
+                    description: `🔁  **|**  Muter muter trus udah di **\`${serverQueue.loop === true ? "enabled" : "disabled"}\`**`
+                }
+            });
+        };
+        return message.channel.send({
+            embed: {
+                color: "RED",
+                description: "masih gelep oe"
+            }
+        });
+    }
+});
+
+async function handleVideo(video, message, voiceChannel, playlist = false) {
+    const serverQueue = queue.get(message.guild.id);
+    const song = {
+        id: video.id,
+        title: Util.escapeMarkdown(video.title),
+        url: `https://www.youtube.com/watch?v=${video.id}`
+    };
+    if (!serverQueue) {
+        const queueConstruct = {
+            textChannel: message.channel,
+            voiceChannel: voiceChannel,
+            connection: null,
+            songs: [],
+            volume: 100,
+            playing: true,
+            loop: false
+        };
+        queue.set(message.guild.id, queueConstruct);
+        queueConstruct.songs.push(song);
+
+        try {
+            var connection = await voiceChannel.join();
+            queueConstruct.connection = connection;
+            play(message.guild, queueConstruct.songs[0]);
+        } catch (error) {
+            console.error(`[ERROR] I could not join the voice channel, because: ${error}`);
+            queue.delete(message.guild.id);
+            return message.channel.send({
+                embed: {
+                    color: "RED",
+                    description: `gw ga bisa join vc karena: **\`${error}\`**`
+                }
+            });
+        }
+    } else {
+        serverQueue.songs.push(song);
+        if (playlist) return;
+        else return message.channel.send({
+            embed: {
+                color: "GREEN",
+                description: `✅  **|**  **\`${song.title}\`** udah gw masukin queue bos`
+            }
+        });
+    }
+    return;
+}
+
+function chunk(array, chunkSize) {
+    const temp = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        temp.push(array.slice(i, i + chunkSize));
+    }
+    return temp;
+}
+
+function play(guild, song) {
+    const serverQueue = queue.get(guild.id);
+
+    if (!song) {
+        serverQueue.voiceChannel.leave();
+        return queue.delete(guild.id);
+    }
+
+    const dispatcher = serverQueue.connection.play(ytdl(song.url))
+        .on("finish", () => {
+            const shiffed = serverQueue.songs.shift();
+            if (serverQueue.loop === true) {
+                serverQueue.songs.push(shiffed);
+            };
+            play(guild, serverQueue.songs[0]);
+        })
+        .on("error", error => console.error(error));
+    dispatcher.setVolume(serverQueue.volume / 100);
+
+    serverQueue.textChannel.send({
+        embed: {
+            color: "BLUE",
+            description: `🎶  **|**  Mulai mainin: **\`${song.title}\`**`
+        }
+    });
+}
+
+bot.login(process.env.BOT_TOKEN);
+
+process.on("unhandledRejection", (reason, promise) => {
+    try {
+        console.error("Unhandled Rejection at: ", promise, "reason: ", reason.stack || reason);
+    } catch {
+        console.error(reason);
+    }
+});
+
+process.on("uncaughtException", err => {
+    console.error(`Caught exception: ${err}`);
+    process.exit(1);
+});
